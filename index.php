b3cc273a507ea193a15468e76445281826ff1480
eliott11 <55191511+eliott11@users.noreply.github.com>
2020-12-08 16:05:22 +0100

    add content accueil + contact page
    Add content on accueil page w/ CSS + contact page w/ CSS

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php include 'header.php'; ?>
 
     <body>
+        <img src="IMG/logo-vert.png" class="img-logo-vert">
         <div class="accueil-title-a">
             <h1 class="accueil-title-a-b">PARROT</h1>
         </div>
@@ -9,6 +10,14 @@
         </div>
         <div>
             <p class="accueil-subtitle">French digital design agency based in Paris</p>
+        </div>
+
+        <div>
+            <p class="accueil-subtitle2">French digital design agency based in Paris</p>
+        </div>
+        <div>
+            <p class="accueil-text" align="center">Parrot Agency is creative online, web and mobile software development company. Our office is based in Paris. 
+                Our company offers services like design website, development and visual identity.</p>
         </div>
     </body>
 
