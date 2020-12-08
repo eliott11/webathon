d7871603efc5f78060f6c573a411942ff76084a3
eliott11 <55191511+eliott11@users.noreply.github.com>
2020-12-08 16:58:41 +0100

    add text box + fix css

--- index.php
+++ index.php
@@ -30,6 +30,31 @@
             <p class="accueil-text">Parrot Agency is creative online, web and mobile software development company. Our office is based in Paris. 
                 Our company offers services like design website, development and visual identity.</p>
         </div>
+        
+
+        <!---- Text + box part ---->
+        <svg class="box-text" xmlns="http://www.w3.org/2000/svg" width="1628" height="778" viewBox="0 0 1628 778">
+            <g id="Groupe_2" data-name="Groupe 2" transform="translate(-27.016 -146)">
+                <rect id="Rectangle_1" data-name="Rectangle 1" width="724" height="481" transform="translate(27.016 146)" fill="#936f9b"/>
+                <text id="Brand_Design" data-name="Brand Design" transform="translate(62.016 260)" fill="#ffe9f3" font-size="70" font-family="Poppins-Regular, Poppins"><tspan x="0" y="0">Brand Design</tspan></text>
+                <text id="Lorem_Ipsum_is_simply_dummy_text_of_the_printing_and_typesetting_industry._Lorem_Ipsum_has_been_the_industry_s_standard_dummy_text_ever_since_the_1500s_when_an_unknown_printer_" data-name="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer
+            " transform="translate(62.016 476)" fill="#ffe9f3" font-size="20" font-family="ArialMT, Arial"><tspan x="0" y="18">Lorem Ipsum is simply dummy text of the printing and typesetting </tspan><tspan x="0" y="41">industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever </tspan><tspan x="0" y="64">since the 1500s, when an unknown printer</tspan><tspan font-family="HelveticaNeue, Helvetica Neue"><tspan x="0" y="87"></tspan></tspan></text>
+                <text id="_-_Brand_Identity_-_Brand_Story_-_Company_purpose" data-name="- Brand Identity
+            - Brand Story
+            - Company purpose" transform="translate(62.016 334)" fill="#ffe9f3" font-size="30" font-family="Poppins-Regular, Poppins"><tspan x="0" y="0">- Brand Identity</tspan><tspan x="0" y="46">- Brand Story</tspan><tspan x="0" y="92">- Company purpose</tspan></text>
+            </g>
+            <g id="Groupe_3" data-name="Groupe 3" transform="translate(842 174)">
+                <rect id="Rectangle_2" data-name="Rectangle 2" width="724" height="481" transform="translate(62 123)" fill="#bbddca"/>
+                <text id="Experience_Design" data-name="Experience Design" transform="translate(107 229)" fill="#87b6a7" font-size="70" font-family="Poppins-Regular, Poppins"><tspan x="0" y="0">Experience Design</tspan></text>
+                <text id="Lorem_Ipsum_is_simply_dummy_text_of_the_printing_and_typesetting_industry._Lorem_Ipsum_has_been_the_industry_s_standard_dummy_text_ever_since_the_1500s_when_an_unknown_printer_2" data-name="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer
+            " transform="translate(107 445)" fill="#87b6a7" font-size="20" font-family="ArialMT, Arial"><tspan x="0" y="18">Lorem Ipsum is simply dummy text of the printing and typesetting </tspan><tspan x="0" y="41">industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever </tspan><tspan x="0" y="64">since the 1500s, when an unknown printer</tspan><tspan font-family="HelveticaNeue, Helvetica Neue"><tspan x="0" y="87"></tspan></tspan></text>
+                <text id="_-_UX_UI_-_Wireframe_prototypes_-_User_flows_persona" data-name="- UX/UI
+            - Wireframe, prototypes
+            - User flows, persona" transform="translate(107 303)" fill="#87b6a7" font-size="30" font-family="Poppins-Regular, Poppins"><tspan x="0" y="0">- UX/UI</tspan><tspan x="0" y="46">- Wireframe, prototypes</tspan><tspan x="0" y="92">- User flows, persona</tspan></text>
+            </g>
+        </svg>
+
+
     </body>
 
 <?php include 'footer.php'; ?>
