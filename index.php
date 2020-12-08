16d9a3447a32ade9adbe2c3501223c0bb4167ea8
eliott11 <55191511+eliott11@users.noreply.github.com>
2020-12-08 15:36:24 +0100

    Add accueil page

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 <?php include 'header.php'; ?>
 
     <body>
-        
+        <div class="accueil-title-a">
+            <h1 class="accueil-title-a-b">PARROT</h1>
+        </div>
+        <div class="accueil-title-b">
+            <h1 class="accueil-title-a-b">AGENCY</h1>
+        </div>
+        <div>
+            <p class="accueil-subtitle">French digital design agency based in Paris</p>
+        </div>
     </body>
 
 <?php include 'footer.php'; ?>
