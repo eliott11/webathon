140c59c711a91242f8117cae70f5ef4f32f2b452
eliott11 <55191511+eliott11@users.noreply.github.com>
2020-12-08 14:46:57 +0100

    setup project

---
+++ header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Parrot</title>
+    <link type="text/css" rel="stylesheet" href="STYLE\style.css">
+</head>
